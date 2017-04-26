f3c8aa02b16dcaaa6cce78cfaeda6992d9b5accb
jvaug <gvaugha@gmail.com>
2017-04-26 11:16:58 -0500

    add blank keys.py template

---
+++ keys.py
@@ -0,0 +1,14 @@
+keys = dict(
+		consumer_key ='',
+		consumer_secret = '',
+
+		access_token = '',
+		access_token_secret = '',
+
+		stream_rule = '@USERNAME',
+		account_screen_name = 'USERNAME',
+		account_user_id = '',
+
+		torch_rnn_path = '',
+		checkpoint_file_path = ''
+		)
